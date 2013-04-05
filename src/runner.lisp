9aca2c1bb95cf1634915b4ee9905c962250b426e
Russell Sim <russell.sim@gmail.com>
2013-04-05 18:58:15 +1100

    Added trace function.

--- src/runner.lisp
+++ src/runner.lisp
@@ -19,22 +19,51 @@
 
 (defsynopsis (:postfix "SYSTEMS")
   (text :contents "Run the tests for a common lisp library.")
+  (group (:header "Debugging options:")
+         (flag :short-name "t" :long-name "trace"
+               :description "Trace function calls."))
   (group (:header "Immediate exit options:")
-    (flag :short-name "h" :long-name "help"
-      :description "Print this help and exit.")
-    (flag :short-name "v" :long-name "version"
-          :description "Print version number and exit.")))
+         (flag :short-name "h" :long-name "help"
+               :description "Print this help and exit.")
+         (flag :short-name "v" :long-name "version"
+               :description "Print version number and exit.")))
+
+(defun safe-trace (sym package)
+  (handler-bind ((simple-error
+                   #'(lambda (c)
+                       (format t "Skipping Symbol ~A (probably a macro)~%" sym)
+                       (invoke-restart 'skip-symbol))))
+    (if (and (fboundp sym)
+             (functionp (symbol-function sym))
+             (equal (package-name (symbol-package sym))
+                    (symbol-name package)))
+        (progn
+          (format t "Tracing symbol ~A~%" sym)
+          (restart-case
+              (eval `(trace ,(intern (symbol-name sym) package)))
+            (skip-symbol () nil)))
+        (format t "Not tracing Symbol ~A~%" sym))))
+
+(defun trace-package (package)
+  (do-symbols (sym package)
+    (safe-trace sym package)))
 
 (defun main (&rest arguments)
   (make-context :cmdline arguments)
-  (when (getopt :short-name "h")
-    (help)
-    (exit))
-  (when (> (length (remainder)) 1)
-    (format t "Error, only one system allowed.~%")
-    (help)
-    (exit))
-  (asdf:oos 'asdf:test-op (intern (string-upcase (car (remainder))) "KEYWORD")))
+  (flet ((considered-exit () (help) (exit)))
+    (when (getopt :short-name "h") (considered-exit))
+    (cond
+      ((> (length (remainder)) 1)
+       (format t "ERROR, only one system allowed.~%")
+       (considered-exit))
+      ((< (length (remainder)) 1)
+       (format t "ERROR, no system found.  At least one must be specified.~%")
+       (considered-exit))))
+  (let ((package (intern (string-upcase (car (remainder))))))
+    (asdf:oos 'asdf:load-op package)
+    (when (getopt :short-name "t")
+      (trace-package package))
+    (asdf:oos 'asdf:test-op package)))
 
 (defun entry-point ()
   (apply 'main "test-op" asdf/image:*command-line-arguments*))
